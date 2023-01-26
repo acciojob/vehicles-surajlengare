55a31800d73e52753ba92dd247cf82b18f817376
surajlengare <suraj.lengare@rsllearning.com>
2023-01-26 16:04:09 +0530

    Answer 1

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,41 @@
+package com.driver;
+
+public class Boat implements WaterVehicle
+{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+                // getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+                    // functions start
+
+    public String getVehicleName()
+    {
+        return this.name;
+    }
+
+    public int getVehicleCapacity()
+    {
+        return this.capacity;
+    }
+}
